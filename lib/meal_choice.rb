c96740e689fd2bb20bbef07f5ca5df1fcd54126f
Neal JIn Oh <no4259@gmail.com>
2019-08-14 15:49:16 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,8 @@
 # Your code here
 def meal_choice(veg1, veg2, protein = "meat")
   p "A plate of #{protein} with #{veg1} and #{veg2}."
+  return 
+  
 end
 
 meal_choice("broccoli", "macaroni", "tofu")
